1e886fd914eaacf828f629582a8341609bdcc7ec
dmplna <dmtrv.pln@gmail.com>
2026-03-05 04:13:33 +0700

    изменения upload.html

--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -80,13 +80,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+USE_EXTERNAL_DB = os.getenv('USE_EXTERNAL_DB', 'False') == 'True'
 
+if USE_EXTERNAL_DB:
+    DATABASES = {
+        'default': {
+            'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
+            'NAME': os.getenv('DB_NAME', ''),
+            'USER': os.getenv('DB_USER', ''),
+            'PASSWORD': os.getenv('DB_PASSWORD', ''),
+            'HOST': os.getenv('DB_HOST', ''),
+            'PORT': os.getenv('DB_PORT', '3306'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -140,5 +153,3 @@ MEDIA_URL = '/media/'
 # Физический путь на диске, где будет создана папка media
 # BASE_DIR — это папка, где лежит manage.py
 MEDIA_ROOT = BASE_DIR / 'media'
-
-# dotenv уже загружен в начале файла
